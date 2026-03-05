7480ea6cf2a1280edf1d7625c7ce92beb0f3099c
electric-visa <visa@electric-visa.net>
2026-03-05 13:19:00 +0200

    Initial Xcode project setup

---
+++ YoloDL/YoloDLApp.swift
@@ -0,0 +1,17 @@
+//
+//  YoloDLApp.swift
+//  YoloDL
+//
+//  Created by Visa Uotila on 5.3.2026.
+//
+
+import SwiftUI
+
+@main
+struct YoloDLApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
