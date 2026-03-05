f66b744c993daf7caf16c8d62059fc3bb07394f3
electric-visa <visa@electric-visa.net>
2026-03-05 13:49:48 +0200

    Added URL text field and download button.

--- YoloDL/ContentView.swift
+++ YoloDL/ContentView.swift
@@ -8,13 +8,17 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var url: String = ""
     var body: some View {
         VStack {
             Image(systemName: "globe")
                 .imageScale(.large)
                 .foregroundStyle(.tint)
             Text("YoloDL 0.01")
-        }
+            TextField("Enter source URL", text: $url)
+            Button("Download"){ print(url)
+            }
+            }
         .padding()
     }
 }
